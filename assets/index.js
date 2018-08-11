8dc0fdba38659996dbde5c68973a832d1426cc07
aliciapflaumer <alpfla7@gmail.com>
2018-08-11 16:48:51 -0400

    Created footer
    Added my name to the bottom of page

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,14 @@
-let video = document.getElementById('video')
-let question = document.getElementById('question')
+let video = document.getElementByClass('video')
+let question = document.getElementByClass('question')
+let play = document.getElementByClass('play-video')
+let pause = document.getElementByClass('pause-video')
+
+$('play').click(function() {
+
+})
+
+$('pause').click(function() {
+  $('question').show()
+})
 
 // At 0:50 seconds, pause video, show question and multiple choice answers
